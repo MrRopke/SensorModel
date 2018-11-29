f5aadd7d6c755d07add590e2b3d6b50bd2e54137
MrRopke <Theisropke@gmail.com>
2018-11-29 10:06:24 +0100

    Main

---
+++ SensorModels/Sensor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SensorModels
+{
+    public class Sensor
+    {
+        public string MacAddress { get; set; }
+        public string Name { get; set; }
+        public double LimitUp { get; set; }
+        public double LimitLow { get; set; }
+        public int FK_UserId { get; set; }
+        public SensorData Data { get; set; }
+    }
+}
---
+++ SensorModels/SensorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorModels
+{
+    public class SensorData
+    {
+        public int Id { get; set; }
+        public double Humidity { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string FK_MacAddress { get; set; }
+    }
+}
