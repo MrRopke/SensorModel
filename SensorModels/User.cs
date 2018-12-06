ff73aadf63fbc0ed40c0f59e39e88fbb22580f53
cybon <kimon5@hotmail.dk>
2018-12-06 14:03:03 +0100

    asd

---
+++ SensorModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
