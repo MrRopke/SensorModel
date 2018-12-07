9301307eaaa43b5403a3afb0fbb58ea04b6ffd17
MrRopke <Theisropke@gmail.com>
2018-12-07 11:49:23 +0100

    neeww

---
+++ SensorModels/PiPortModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorModels
+{
+    public class PiPortModel
+    {
+        public string MacAddress { get; set; }
+        public int Port { get; set; }
+    }
+}
---
+++ SensorModels/Watering.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SensorModels
+{
+    public class Watering
+    {
+        public int Port { get; set; }
+        public int Water { get; set; }
+    }
+}
